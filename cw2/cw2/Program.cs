013d3b20309aee6a8c1f8427851f5ce20d7ecc71
s11283 <s11283@pjwstk.edu.pl>
2020-11-19 05:24:21 +0100

    XML serialisation Ready

--- cw2/cw2/Program.cs
+++ cw2/cw2/Program.cs
@@ -1,4 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Xml;
+using System.Xml.Serialization;
 
 namespace cw2
 {
@@ -6,7 +11,71 @@ namespace cw2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string dane, wynik, typ;
+            List<Student> studentData = new List<Student>();
+
+            
+            
+            using (var stream = new StreamReader(File.OpenRead("dane.csv"))) {
+                string line = null;
+                while ((line = stream.ReadLine()) != null) {
+
+                    string[] student = line.Split(',');
+                    
+                    var st = new Student
+                    {
+                        Imie = student[0],
+                        Nazwisko = student[1],
+                        Kierunek = student[2],
+                        Mode = student[3],
+                        numerIndeksu = student[4],
+                        dataUrodzenia = student[5],
+                        email = student[6],
+                        imieMatki = student[7],
+                        imieOjca = student[8]
+
+                    };
+                    studentData.Add(st);
+                }
+            }
+          
+
+                FileStream writer = new FileStream(@"żesult.xml", FileMode.Create);
+                    
+                XmlSerializer serializer = new XmlSerializer(typeof(List<Student>));
+                serializer.Serialize(writer, studentData);
+         
+
+
+            
+            
+
         }
+
+ 
+        
+        
+//methods---------------------------------
+
+              
     }
 }
+//---------weryfikacja parametrów
+/*if (args == null || args.Length < 3)
+{
+    dane = "dane.csv";
+    wynik = "żesult.xml";
+    typ = "xml";
+}
+else
+{
+    dane = args[0];
+    wynik = args[1];
+    typ = args[2];
+    if (typ != "xml")
+    {
+        typ = "xml"; //weryfikacja warunku - na razie tylko xml
+        Console.Write("\npoprawiono trzeci parametr (typ danych) na xml");
+    }
+}*/
+//-----------------------------
---
+++ cw2/cw2/Student.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Xml.Serialization;
+
+namespace cw2
+{
+    [Serializable]
+    public class Student
+    {
+
+        [XmlElement(ElementName = "Imie")]
+        public string Imie { get; set; }
+        [XmlElement(ElementName = "Nazwisko")]
+        public string Nazwisko { get; set; }
+        [XmlElement(ElementName = "Studia")]
+        public string Kierunek { get; set; }
+        [XmlElement(ElementName = "Mode")]
+        public string Mode { get; set; }
+        [XmlElement(ElementName = "Indeks")]
+        public string numerIndeksu { get; set; }
+        [XmlElement(ElementName = "dataUrodzenia")]
+        public string dataUrodzenia { get; set; }
+        [XmlElement(ElementName = "email")]
+        public string email { get; set; }
+        [XmlElement(ElementName = "Imie matki")]
+        public string imieMatki { get; set; }
+        [XmlElement(ElementName = "Imie ojca")]
+        public string imieOjca { get; set; }
+
+
+
+    }
+}
